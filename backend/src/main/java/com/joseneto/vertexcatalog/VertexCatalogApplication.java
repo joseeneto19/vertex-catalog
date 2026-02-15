f74eeab1b67ffba3edd430e22d1565a412e0fd2c
joseeneto19 <josemnetodev@gmail.com>
2026-02-14 22:17:10 -0300

    project created

---
+++ backend/src/main/java/com/joseneto/vertexcatalog/VertexCatalogApplication.java
@@ -0,0 +1,13 @@
+package com.joseneto.vertexcatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VertexCatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VertexCatalogApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/joseneto/vertexcatalog/VertexCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joseneto.vertexcatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VertexCatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
