9177a0de59a81d98c6d8db0f867fbbdab6dffe72
joseeneto19 <josemnetodev@gmail.com>
2026-02-16 23:52:56 -0300

    feat: add repository

--- backend/src/main/java/com/joseneto/vertexcatalog/repositories/CategoryRepository.java
+++ backend/src/main/java/com/joseneto/vertexcatalog/repositories/CategoryRepository.java
@@ -1,4 +1,9 @@
 package com.joseneto.vertexcatalog.repositories;
 
-public interface CategoryRepository {
+import com.joseneto.vertexcatalog.entities.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
 }
