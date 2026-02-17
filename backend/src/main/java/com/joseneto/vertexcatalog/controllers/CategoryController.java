b4d25f84ec94d751095647d1a4d9bd3a83fb6610
joseeneto19 <josemnetodev@gmail.com>
2026-02-16 23:53:24 -0300

    feat: add endpoint and method findall

--- backend/src/main/java/com/joseneto/vertexcatalog/controllers/CategoryController.java
+++ backend/src/main/java/com/joseneto/vertexcatalog/controllers/CategoryController.java
@@ -1,4 +1,26 @@
 package com.joseneto.vertexcatalog.controllers;
 
+import com.joseneto.vertexcatalog.entities.Category;
+import com.joseneto.vertexcatalog.servicies.CategoryService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/categories")
 public class CategoryController {
+
+    private CategoryService categoryService;
+
+    public CategoryController(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Category>> findAll() {
+        return ResponseEntity.ok(categoryService.findAll());
+    }
 }
--- backend/src/main/java/com/joseneto/vertexcatalog/servicies/CategoryService.java
+++ backend/src/main/java/com/joseneto/vertexcatalog/servicies/CategoryService.java
@@ -1,4 +1,22 @@
 package com.joseneto.vertexcatalog.servicies;
 
+import com.joseneto.vertexcatalog.entities.Category;
+import com.joseneto.vertexcatalog.repositories.CategoryRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class CategoryService {
+
+    private final CategoryRepository categoryRepository;
+
+    public CategoryService(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+    public List<Category> findAll() {
+        return categoryRepository.findAll();
+    }
+
 }
