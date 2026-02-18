d09f3c01aae8fb301ce1f07a42c5bc7db16ad6c1
joseeneto19 <josemnetodev@gmail.com>
2026-02-18 13:24:53 -0300

    feat: create method and endpoint insert

--- backend/src/main/java/com/joseneto/vertexcatalog/controllers/CategoryController.java
+++ backend/src/main/java/com/joseneto/vertexcatalog/controllers/CategoryController.java
@@ -2,12 +2,14 @@ package com.joseneto.vertexcatalog.controllers;
 
 import com.joseneto.vertexcatalog.dto.CategoryDTO;
 import com.joseneto.vertexcatalog.services.CategoryService;
+import jakarta.servlet.ServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -28,5 +30,13 @@ public class CategoryController {
     @GetMapping("/{id}")
     public ResponseEntity<CategoryDTO> findById(@PathVariable Long id) {
         return ResponseEntity.ok(categoryService.findById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<CategoryDTO> insert(@RequestBody CategoryDTO dto) {
+        dto = categoryService.insert(dto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(dto.getId()).toUri();
+        return ResponseEntity.created(uri).body(dto);
     }
 }
